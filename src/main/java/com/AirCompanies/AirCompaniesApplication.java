a40cb8caf667a6ba7ecf12a500f85f3a4bba4b23
AndriyHishchak <babnikgo@gmail.com>
2021-02-25 10:50:35 +0200

    Initial commit

---
+++ src/main/java/com/AirCompanies/AirCompaniesApplication.java
@@ -0,0 +1,13 @@
+package com.AirCompanies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirCompaniesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirCompaniesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AirCompanies/AirCompaniesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AirCompanies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirCompaniesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
